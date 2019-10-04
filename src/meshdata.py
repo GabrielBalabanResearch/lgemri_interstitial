2a193cc8630a31054edd73c1a5cc5b6827dd89e3
Gabriel Balaban <gabrib@simula.no>
2019-10-04 14:39:29 +0200

    update source

--- src/meshdata.py
+++ src/meshdata.py
@@ -48,25 +48,11 @@ class MeshData(object):
 		facets = np.vstack([np.roll(self.elems, i, axis = 1).flatten() for i in range(self.edim - 1)]).transpose()
 		return npi.unique(np.sort(facets, axis = 1))
 
-	#def save(self, path, mode = "a"):
-	# 	meshformat_str = path.split(".")[-1]
-	# 	from formats import FORMATS
-	# 	FORMATS[meshformat_str]().write(path, self, mode = mode)
-
-	# def get_boundary(self):
-	# 	import dolfin as df
-	# 	from meshtoolz.formats.dolfin_format import DolfinFormat
-	# 	dolfin_mesh = DolfinFormat().make_mesh(self)
-
-	# 	bmesh = df.BoundaryMesh(dolfin_mesh, "exterior")
-	# 	vb_v = np.array(bmesh.entity_map(0), dtype = int) #boundary vertex to interior vertex
-	# 	return MeshData(bmesh.coordinates(),
-	# 					bmesh.cells(),
-	# 					boundary_map = vb_v)
-
 	def __str__(self):
 		return "{}D MeshData with {} verticies and {} elements".format(self.vdim, len(self.verts), len(self.elems))
 
+
+#To use some other mesh formats simply make another class like this with "read" and "write" methods. 
 
 class H5Format(object):
 	def parse_path(self, path):
